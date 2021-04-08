7ca5e36847a45723f1540e021162e03e0671336c
LACC2311 <la_cepeda@hotmail.com>
2021-04-08 04:33:04 -0500

    The builtin datatypes in Kotlin

---
+++ src/com/example/datatypes/javacode/DummyClass.java
@@ -0,0 +1,9 @@
+package com.example.datatypes.javacode;
+
+public class DummyClass {
+
+    public String isVacationTime(boolean onVacation) {
+        return onVacation ? "I'm on vacation":"I'm working";
+    }
+
+}
