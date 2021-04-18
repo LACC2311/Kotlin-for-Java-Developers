dac01baaa03858354d5e827fcae0b53ff902c5ea
LACC2311 <la_cepeda@hotmail.com>
2021-04-18 16:34:08 -0500

    Null references in Kotlin

---
+++ src/com/example/datatypes/javacode/NullReferences.java
@@ -0,0 +1,13 @@
+package com.example.datatypes.javacode;
+
+import java.util.Locale;
+
+public class NullReferences {
+
+    public static void main(String[] args) {
+        /*We obtain a null pointer exception*/
+        String str = null;
+        str.toUpperCase();
+    }
+
+}
