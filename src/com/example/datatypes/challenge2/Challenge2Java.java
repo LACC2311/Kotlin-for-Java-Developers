a84b641e1ef75cd1b8212b18c4a017d314c6bb87
LACC2311 <la_cepeda@hotmail.com>
2021-04-18 21:33:46 -0500

    Kotlin Challenges (Round Two)

---
+++ src/com/example/datatypes/challenge2/Challenge2Java.java
@@ -0,0 +1,10 @@
+package com.example.datatypes.challenge2;
+
+public class Challenge2Java {
+
+    public static void method1(char[] charArray) {
+        for(char character: charArray){
+            System.out.println(character);
+        }
+    }
+}
